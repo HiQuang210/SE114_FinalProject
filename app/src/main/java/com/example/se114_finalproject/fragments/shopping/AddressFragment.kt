593a4bf65aae41b3a88c1554e0b1fe2ef68a3556
Quang210 <119526513+HiQuang210@users.noreply.github.com>
2024-12-25 09:37:43 +0700

    Added Address View

---
+++ app/src/main/java/com/example/se114_finalproject/fragments/shopping/AddressFragment.kt
@@ -0,0 +1,100 @@
+package com.example.se114_finalproject.fragments.shopping
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Toast
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.lifecycleScope
+import androidx.lifecycle.repeatOnLifecycle
+import androidx.navigation.fragment.findNavController
+import com.example.se114_finalproject.data.Address
+import com.example.se114_finalproject.databinding.FragmentAddressBinding
+import com.example.se114_finalproject.utilities.Resource
+import com.example.se114_finalproject.viewmodel.AddressVM
+import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.flow.collectLatest
+import kotlinx.coroutines.launch
+
+@AndroidEntryPoint
+class AddressFragment : Fragment() {
+    private lateinit var binding: FragmentAddressBinding
+    val viewModel by viewModels<AddressVM>()
+    //val args by navArgs<AddressFragmentArgs>()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        viewLifecycleOwner.lifecycleScope.launch {
+            viewLifecycleOwner.lifecycle.repeatOnLifecycle(Lifecycle.State.STARTED) {
+                viewModel.addNewAddress.collectLatest {
+                    when (it) {
+                        is Resource.Loading -> {
+                            binding.progressbarAddress.visibility = View.VISIBLE
+                        }
+                        is Resource.Success -> {
+                            binding.progressbarAddress.visibility = View.INVISIBLE
+                            findNavController().navigateUp()
+                        }
+                        is Resource.Error -> {
+                            binding.progressbarAddress.visibility = View.INVISIBLE
+                            Toast.makeText(requireContext(), it.message, Toast.LENGTH_SHORT).show()
+                        }
+                        else -> Unit
+                    }
+                }
+            }
+        }
+
+        viewLifecycleOwner.lifecycleScope.launch {
+            viewLifecycleOwner.lifecycle.repeatOnLifecycle(Lifecycle.State.STARTED) {
+                viewModel.error.collectLatest {
+                    Toast.makeText(requireContext(), it, Toast.LENGTH_SHORT).show()
+                }
+            }
+        }
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?,
+    ): View {
+        binding = FragmentAddressBinding.inflate(inflater)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+//        val address = args.address
+//        if (address == null) {
+//            binding.buttonDelete.visibility = View.GONE
+//        } else {
+//            binding.apply {
+//                edAddressTitle.setText(address.addressTitle)
+//                edFullName.setText(address.fullName)
+//                edState.setText(address.street)
+//                edPhone.setText(address.phone)
+//                edCity.setText(address.city)
+//                edState.setText(address.state)
+//            }
+//        }
+
+        binding.apply {
+            buttonSave.setOnClickListener {
+                val addressTitle = edAddressTitle.text.toString()
+                val fullName = edFullName.text.toString()
+                val street = edStreet.text.toString()
+                val phone = edPhone.text.toString()
+                val city = edCity.text.toString()
+                val state = edState.text.toString()
+                val address = Address(addressTitle, fullName, street, phone, city, state)
+
+                viewModel.addAddress(address)
+            }
+        }
+    }
+}
